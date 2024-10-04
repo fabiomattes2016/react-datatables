9a78525321765b788523123d83c068495c0a0265
Fabio Jesus da Silva Mattes <fabio_mattes@sicredi.com.br>
2024-10-04 11:46:30 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DataTable from './components/DataTable';
+
+interface Data {
+  name: string;
+  age: number;
+  country: string;
+}
 
 function App() {
+  const columns = [
+    { header: 'Nome', acessor: 'name' as keyof Data },
+    { header: 'Idade', acessor: 'age' as keyof Data },
+    { header: 'País', acessor: 'country' as keyof Data },
+  ];
+
+  const data: Data[] = [
+    { name: 'João', age: 38, country: 'Brasil'  },
+    { name: 'Maria', age: 19, country: 'Espanha'  },
+    { name: 'Pedro', age: 50, country: 'Uruguai'  },
+    { name: 'Antonio', age: 70, country: 'Uruguai'  },
+    { name: 'Roberto', age: 32, country: 'Argentina'  },
+    { name: 'José', age: 40, country: 'Brasil'  },
+    { name: 'Tiago', age: 45, country: 'Brasil'  },
+    { name: 'Marcos', age: 42, country: 'Brasil'  },
+    { name: 'Paulo', age: 40, country: 'Brasil'  },
+    { name: 'Andrá', age: 50, country: 'Brasil'  },
+  ];
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{padding: '20px'}}>
+        <h1>Tabela estilo DataTables sem uso de bibliotecas</h1>
+        <DataTable columns={columns} data={data} />
+      </div>
     </div>
   );
 }
---
+++ src/components/DataTable/index.tsx
@@ -0,0 +1,131 @@
+import {useMemo, useState} from "react";
+
+interface DataTableProps<T> {
+  columns: Array<{header: string, acessor: keyof T}>;
+  data: T[];
+}
+
+const DataTable = <T extends object>({ columns, data }: DataTableProps<T>) => {
+  const [filter, setFilter] = useState<string>('');
+  const [sortColumn, setSortColumn] = useState<keyof T | null>(null);
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 5;
+
+  // Filtrar os dados
+  const filteredData = useMemo(() => {
+    return data.filter(
+      (row) => Object.values(row).some(
+        (value) => value
+          .toString()
+          .toLowerCase()
+          .includes(filter.toLowerCase())
+      ));
+  }, [data, filter]);
+
+  // Ordenar os campos
+  const sortedData = useMemo(() => {
+    if (!sortColumn) return filteredData;
+
+    const sorted = [...filteredData].sort((a, b) => {
+      const aValue = a[sortColumn];
+      const bValue = b[sortColumn];
+
+      if(aValue < bValue) return sortDirection === 'asc' ? -1 : 1;
+      if(aValue > bValue) return sortDirection === 'asc' ? 1 : -1;
+
+      return 0;
+    });
+
+    return sorted;
+  }, [filteredData, sortColumn, sortDirection]);
+
+  // Paginar os dados
+  const paginatedData = useMemo(() => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+
+    return sortedData.slice(startIndex, startIndex + itemsPerPage);
+  }, [sortedData, currentPage]);
+
+  // Trocar a coluna de ordenação
+  const handleSort = (column: keyof T) => {
+    if (sortColumn === column) {
+      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortColumn(column);
+      setSortDirection('asc');
+    }
+  }
+
+  // Trocar de página
+  const handlePageChange = (page: number) => {
+    if (page < 1 || page > Math.ceil(sortedData.length / itemsPerPage))
+      return;
+
+    setCurrentPage(page);
+  }
+
+  return(
+    <div>
+      {/* Campo de filtro */}
+      <input 
+        type="text"
+        placeholder="Buscar..."
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        style={{marginBottom: '10px', padding: '5px'}}
+      />
+
+      {/* Tabela */}
+      <table style={{width: '100%', borderCollapse: 'collapse'}}>
+        <thead>
+          <tr>
+            {columns.map((column) => (
+              <th 
+                key={column.acessor as string}
+                style={{padding: '10px', borderBottom: '1px solid black', cursor: 'pointer'}}
+                onClick={() => handleSort(column.acessor)}
+              >
+                {column.header}
+                {
+                  sortColumn === column.acessor && (sortDirection === 'asc' ?  ' 🔼': ' 🔽')
+                }
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {paginatedData.map((row, index) => (
+            <tr key={index}>
+              {columns.map((column) => (
+                <td
+                  key={column.acessor as string}
+                  style={{padding: '10px', borderBottom: '1px solid lightgray'}}
+                >
+                  {row[column.acessor] as string}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* Paginação */}
+      <div style={{marginTop: '10px'}}>
+          <button 
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >⬅️</button>
+          <span style={{margin: '0 10px'}}>Página {currentPage} de {Math.ceil(sortedData.length / itemsPerPage)}</span>
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === Math.ceil(sortedData.length / itemsPerPage)}
+          >
+            ➡️
+          </button>
+      </div>
+    </div>
+  );
+}
+
+export default DataTable;
